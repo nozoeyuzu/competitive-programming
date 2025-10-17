62307e291dd27c3f4e4b23996bf6108c968ec7fc
nozoeyuzu <citron12yz21n82@docomo.ne.jp>
2025-10-17 15:45:27 +0900

    ABC424_A

--- 20251017_atcoder.py
+++ 20251017_atcoder.py
@@ -1,12 +1,20 @@
 #ABC425_A
 
-N = int(input())
-result = 0
+# N = int(input())
+# result = 0
 
-for i in range(N+1):
-    if i % 2 == 0:
-        result += i**3
-    else:
-        result -= i**3
+# for i in range(N+1):
+#     if i % 2 == 0:
+#         result += i**3
+#     else:
+#         result -= i**3
 
-print(result)
+# print(result)
+
+#ABC424_A
+
+a,b,c = map(int, input().split())
+if a!=b and b!=c and c!=a:
+    print('No')
+else:
+    print('Yes')
