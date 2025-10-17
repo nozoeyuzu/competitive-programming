59e1dc25c9177d5a7e36e37a371caabf590ab37e
nozoeyuzu <citron12yz21n82@docomo.ne.jp>
2025-10-17 16:42:54 +0900

    1017

--- 20251017_atcoder.py
+++ 20251017_atcoder.py
@@ -13,8 +13,15 @@
 
 #ABC424_A
 
-a,b,c = map(int, input().split())
-if a!=b and b!=c and c!=a:
-    print('No')
-else:
-    print('Yes')
+# a,b,c = map(int, input().split())
+# if a!=b and b!=c and c!=a:
+#     print('No')
+# else:
+#     print('Yes')
+
+#ABC423_A
+X, C = map(int, input().split())
+max = (X * 1000) // (1000 + C)
+max = (max // 1000) * 1000
+
+print(max)
