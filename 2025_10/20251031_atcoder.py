a39c63b5891df49aa67b31fd979f2df09c7aa831
nozoeyuzu <citron12yz21n82@docomo.ne.jp>
2025-10-31 12:07:12 +0900

    ABC414_B

--- 2025_10/20251031_atcoder.py
+++ 2025_10/20251031_atcoder.py
@@ -9,3 +9,22 @@
 #         count += 1
 
 # print(count)
+
+# N = int(input())
+# c_list = []
+# l_list = []
+# for _ in range(N):
+#     c, l = input().split()
+#     c_list.append(c)
+#     l_list.append(int(l))
+
+# S = []
+# total_length = 0
+# for i in range(N):
+#     total_length += l_list[i]
+#     if total_length > 100:
+#         print('Too Long')
+#         exit()
+#     S.append(c_list[i]*l_list[i])
+
+# print(''.join(S))
