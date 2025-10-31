6870da85c679f05855955df2d78245e3bbdeddab
nozoeyuzu <citron12yz21n82@docomo.ne.jp>
2025-10-31 11:35:13 +0900

    ABC414_A

--- 2025_10/20251031_atcoder.py
+++ 2025_10/20251031_atcoder.py
@@ -1,1 +1,11 @@
 #ABC414
+# N, L, R = map(int, input().split())
+# XY = [tuple(map(int, input().split())) for _ in range(N)]
+# X,Y = zip(*XY)
+# count = 0
+
+# for i in range(N):
+#     if X[i] <= L and Y[i] >= R:
+#         count += 1
+
+# print(count)
