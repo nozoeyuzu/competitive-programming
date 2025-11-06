e34875074d456615ab8c432a66376e4434bd74ef
nozoeyuzu <citron12yz21n82@docomo.ne.jp>
2025-11-06 12:18:41 +0900

    1106

--- 20251106_atcoder.py
+++ 20251106_atcoder.py
@@ -2,3 +2,10 @@
 # A, B = map(int, input().split())
 # print((A+B//2)//B)
 
+X, Y = map(int, input().split())
+count = 0
+for i in range(1, 7):
+    for j in range(1, 7):
+        if i+j >= X or abs(i-j) >= Y:
+            count += 1
+print(count/36)
