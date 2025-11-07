dd938726c66ba7aace8161a15b5acf8dbe2a48cb
nozoeyuzu <citron12yz21n82@docomo.ne.jp>
2025-11-07 17:41:50 +0900

    ABC405

--- 20251107_atcoder.py
+++ 20251107_atcoder.py
@@ -6,3 +6,31 @@
 #     print('Yes')
 # else:
 #     print('No')
+
+# N, M = map(int, input().split())
+# A = list(map(int, input().split()))
+# B = [i for i in range(1,M+1)]
+# count = 0
+
+# for i in range(N):
+#     if B in A:
+#         A.pop(len(A)-1)
+#         count += 1
+#     else:
+#         print(count)
+#         exit()
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+
+need = set(range(1, M+1))
+if not need.issubset(set(A)):
+    print(0)
+    exit()
+
+first = {}
+for i, x in enumerate(A):
+    if x not in first:
+        first[x] = i
+
+ans = min(N - first[v] for v in range(1, M+1))
+print(ans)
