ccc09dcb78dde9e1f4ab2673d4c5db17fdf8b528
nozoeyuzu <citron12yz21n82@docomo.ne.jp>
2025-11-12 22:05:44 +0900

    1112

--- 20251112_atcoder.py
+++ 20251112_atcoder.py
@@ -4,3 +4,20 @@
 #     print('Success')
 # else:
 #     print('Failure')
+
+N = int(input())
+S = [input() for _ in range(N)]
+login = False
+error = 0
+for s in S:
+    if s == 'login':
+        login = True
+    elif s == 'logout':
+        login = False
+    elif s == 'public':
+        pass
+    elif s == 'private':
+        if login == False:
+            error += 1
+
+print(error)
