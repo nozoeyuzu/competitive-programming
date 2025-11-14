b5010cef2c1be422031eeae43b195c92d5a7307e
nozoeyuzu <citron12yz21n82@docomo.ne.jp>
2025-11-14 16:57:47 +0900

    ABC_400

---
+++ 20251114_atcoder.py
@@ -0,0 +1,16 @@
+#ABC_400
+# A = int(input())
+# if 400 % A == 0:
+#     print(400//A)
+# else:
+#     print(-1)
+
+# N, M = map(int, input().split())
+# ans = 0
+# for i in range(M+1):
+#     ans += N**i
+#     if ans > 1000000000:
+#         print('inf')
+#         exit()
+# print(ans)
+
